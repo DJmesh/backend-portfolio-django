b3c526eb9887ce44177e5c4cad800a100e432cfe
Eduardo Prestes <eduardo.prestes@facens.br>
2025-04-29 11:43:27 -0300

    fix(models): correct user foreign keys to use AUTH_USER_MODEL

--- activity_log/models/activity.py
+++ activity_log/models/activity.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class UserActivity(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     action = models.CharField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
     details = models.TextField(blank=True)
